68a9b21e5c443594e91687191f3dadc6469faa7f
sudharshan-dollu <88039852+sudharshan-dollu@users.noreply.github.com>
2023-11-13 15:36:55 +0530

    fetch posts fix

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -52,10 +52,7 @@ export async function fetchPosts(pageNumber = 1, pageSize=20){
             }
         })
 
-        const totalPostsCount = await Thread.find({ parentId: { $in: [null, undefined] }})
-        // .hint({ parentId: 1 })
-        .lean()
-        .countDocuments();
+        const totalPostsCount = 1
 
         const posts = await fetchPostsQuery.exec()
 
